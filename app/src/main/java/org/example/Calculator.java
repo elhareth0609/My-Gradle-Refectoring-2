e20222a9f5f1c9840a31c5abd0c7550fd439eaa0
elhareth0609 <elhareh0609@gmail.com>
2025-04-06 08:51:02 +0100

    After refectorings 2

--- app/src/main/java/org/example/Calculator.java
+++ app/src/main/java/org/example/Calculator.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class Calculator {
-    public double calculateResult(double a, double b) {
-        double x = a + b;
-        double y = a * b;
-        return x / y;
+    public double calculateResult(double number1, double number2) {
+        double sum = number1 + number2;
+        double product = number1 * number2;
+        return sum / product;
     }
 
-    public void printResult(double res) {
-        System.out.println("Result: " + res);
+    public void printResult(double result) {
+        System.out.println("Result: " + result);
     }
 }
